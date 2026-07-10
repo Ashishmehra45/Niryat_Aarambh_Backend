f109d9d795aa09971618db3ffc294d673e490ea4
Ashish Mehra <ashish6266mehra@gmail.com>
2026-07-10 12:23:08 +0530

    sunny coming

--- routes/Admin/Admin.route.js
+++ routes/Admin/Admin.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const adminController = require("../../controller/Admin/Admin.controller");
 const adminAuthMiddleware = require("../../middlewares/Admin/Admin.middelware");
 
-// Apply the adminAuthMiddleware to all routes below
+// Apply the adminAuthMiddleware to all routes below  
 
 router.post("/register", adminController.adminregister);
 router.post("/login", adminController.adminlogin);
